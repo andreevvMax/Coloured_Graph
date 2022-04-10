1ec0664e3dd89dd541405f84bc4e2daa3f13afc3
andreevvmax <makcim.andreev@list.ru>
2022-04-10 22:37:50 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
+
+vector<vector<int>> Input_Graph(const string& our_file, vector<pair<int, int>>& edges_note, vector<int>& counter)
+{
+    ifstream fin;
+    fin.open(our_file);
+    vector<vector<int>> graph;
+    int ver, edges;
+    vector<pair<int, int>> pairs;
+    edges_note.clear();
+    counter.clear();
+    return graph;
+}
 
 int main()
 {
