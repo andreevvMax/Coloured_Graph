e26b110260ae4b8d64ab6c3a4408252ddca1dd17
andreevvmax <makcim.andreev@list.ru>
2022-04-19 21:24:12 +0300

    third commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <fstream>
 #include <filesystem>
@@ -6,76 +6,104 @@
 
 using namespace std;
 
-vector<vector<int>> Input_Graph(const string& our_file, vector<pair<int, int>>& Edges_note, vector<int>& Ecounter)
-{
+vector<vector<int>> Input_Graph(const string& our_file, vector<pair<int, int>>& Edges_note, vector<int>& e_counter) {
     ifstream fin;
     fin.open(our_file);
     vector<vector<int>> Graph;
-
-    int Verr, Edges;
+    int ver, edg;
     vector<pair<int, int>> pairs;
     Edges_note.clear();
-    Ecounter.clear();
-
-    if (fin.is_open())
-    {
-        fin >> Verr >> Edges;
-        vector <int> V(Verr, 0);
-        Graph.resize(Verr, V);
-
+    e_counter.clear();
+    if (fin.is_open()) {
+        fin >> ver >> edg;
+        vector <int> v(ver, 0);
+        Graph.resize(ver, v);
         auto zero = make_pair(0, 0);
-
         pairs.resize(Graph.size(), zero);
-
-        for (int k = 0; k < Edges; k++)
-        {
-            int first, last;
-
-            fin >> first >> last;
-            auto p1 = make_pair(first, last);
+        for (int k = 0; k < edg; k++) {
+            int start, end;
+            fin >> start >> end;
+            auto p1 = make_pair(start, end);
             Edges_note.push_back(p1);
-            Graph[first][last] = 1;
-            Graph[last][first] = 1;
-            pairs[first].first += 1;
-            pairs[last].first += 1;
-            pairs[first].second = first;
-            pairs[last].second = last;
+            Graph[start][end] = 1;
+            Graph[end][start] = 1;
+            pairs[start].first += 1;
+            pairs[end].first += 1;
+            pairs[start].second = start;
+            pairs[end].second = end;
         }
     }
-    else    cout << "Error" << endl;
-
+    else {
+        cout << "file not read" << endl;
+    }
     sort(pairs.begin(), pairs.end());
-
-    for(int i = pairs.size()-1; i>=0; i--)
-        Ecounter.push_back(pairs[i].second);
-
+    for (int i = pairs.size() - 1; i >= 0; i--) {
+        e_counter.push_back(pairs[i].second);
+    }
     return Graph;
 }
 
-vector<int> solve(vector<vector<int>>& Graph, const vector<int>& Ecounter)
-{
-    vector<int> V_Col(Graph.size(), -1);
-
-    for(auto it : Ecounter)
-    {
+vector<int> solve(vector<vector<int>>& Graph, const vector<int>& e_counter) {
+    vector<int> vColors(Graph.size(), -1);
+    for (auto it : e_counter) {
         int i = it;
-        vector<bool> Is_Col(Graph.size(), 0);
-        for (int j = 0; j < Graph.size(); j++)
-            if (Graph[i][j] == 1)
-                if(V_Col[j] > -1)
-                    Is_Col[V_Col[j]] = 1;
-
-        for (int k = 0; k < Graph.size(); k++)
-            if (!Is_Col[k])
-            {
-                V_Col[i] = k;
+        vector<bool> colored(Graph.size(), false);
+        for (int j = 0; j < Graph.size(); j++) {
+            if (Graph[i][j] == 1) {
+                if (vColors[j] > -1) {
+                    colored[vColors[j]] = true;
+                }
+            }
+        }
+        for (int k = 0; k < Graph.size(); k++) {
+            if (!colored[k]) {
+                vColors[i] = k;
                 break;
             }
+        }
     }
-    return V_Col;
+    return vColors;
 }
 
-int main()
-{
+int main() {
+
+    // путь до директории с тестами и забил названия файлов из это директории в вектор
+
+    string path = "C:/Users/Максим/Desktop/data";
+    auto it = filesystem::directory_iterator(path);
+    vector<filesystem::path> array_path;
+    copy_if(filesystem::begin(it), filesystem::end(it), std::back_inserter(array_path),
+        [](const auto& entry) {
+            return filesystem::is_regular_file(entry);
+        });
+
+    // для каждого файла выполняю тест
+
+    for (auto& p : array_path) {
+        vector<int> e_counter;
+        vector<pair<int, int>> edg;
+        vector<vector<int>> Graph = Input_Graph(p.string(), edg, e_counter);  
+        vector<int> colored2 = solve(Graph, e_counter);
+        int max = -1;
+        for (int i = 0; i < colored2.size(); i++) {
+            if (colored2[i] > max) {
+                max = colored2[i];
+            }
+        }
+        cout << p.string() << " " << max + 1 << endl;
+        /* cout << max + 1 << " " << 0 << endl;
+         for (auto i : colored2) {
+             cout << i << " ";
+         }
+         */
+        cout << endl;
+        for (auto i : edg) {
+            if (colored2[i.first] == colored2[i.second]) {
+                cout << "Error, colored test: " << p.string() << endl;
+                cout << i.first << " " << i.second << endl;
+                break;
+            }
+        }
+    }
     return 0;
 }
